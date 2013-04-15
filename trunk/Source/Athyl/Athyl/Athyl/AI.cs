cfa31728f1629703061ba33413c0364909831cb2
timothy.mattsson <timothy.mattsson@8eb48520-c05a-3f51-4223-508b6fc7d526>
2013-04-15 14:23:08 +0000

    Arv

--- trunk/Source/Athyl/Athyl/Athyl/AI.cs
+++ trunk/Source/Athyl/Athyl/Athyl/AI.cs
@@ -17,68 +17,30 @@ using FarseerPhysics.Collision.Shapes;
 
 namespace Athyl
 {
-    class AI
+    class AI : Player
     {
-        private int framecount;
-        private Texture2D myTexture;
-        private float TimePerFrame;
-        private int Frame;
-        private float TotalElapsed;
-        private int runDirection;
-
-        public DrawableGameObject enemyBody;
-        DrawableGameObject wheel;
-        RevoluteJoint axis;
-        const float speed = 1.0f;
-        Random randomX = new Random(Environment.TickCount);
-        Vector2 jumpForce = new Vector2(0, -2f);
-        DateTime previousJump;
-        const float jumpInterval = 1f;
-
-        public bool OnGround { get; set; }
-        private int numFootContacts;
-
-        bool hit = false;
-        bool seen = false;
-
         //For the normal enemies, the HP should be 100
         public int enemyHP = 100;
-        public AI(World world, Texture2D texture, Vector2 size, float mass, float wheelSize)
+
+        private int runDirection;
+        private DateTime previousJump;
+        private const float jumpInterval = 1f;
+        private bool hit = false;
+        private bool seen = false;
+
+        public AI(World world, Texture2D texture, Vector2 size, Vector2 startPosition, float mass, float wheelSize, Game1 game)
+            : base(world, texture, size, mass, wheelSize, startPosition, game, "enemy")
         {
+            Load(texture, 1, 22, 1);
 
+            //set frame to middle
+            ColFrame = 11;
 
-            Load(texture, 22, 1);
-            Vector2 torsoSize = new Vector2(size.X, size.Y-wheelSize+5);
-
-            //create torso
-            enemyBody = new DrawableGameObject(world, texture, mass, "enemy");
-            enemyBody.Position = new Vector2(randomX.Next(50, 300), 400);
-            enemyBody.body.Restitution = 0;
-
-
-            // Create the feet of the body, here implemented as high friction wheels 
-            wheel = new DrawableGameObject(world, texture, wheelSize, mass / 2.0f, "enemyWheel");
-            wheel.Position = enemyBody.Position + new Vector2(0, torsoSize.Y/2+5);
-            wheel.body.Friction = 3.0f;
-            wheel.body.Restitution = 0;
+            speed = 1f;
+            jumpForce = new Vector2(0, -2f);
             
-
-            // Create a joint to keep the torso upright
-            JointFactory.CreateFixedAngleJoint(world, enemyBody.body);
-
-            // Connect the feet to the torso
-            axis = JointFactory.CreateRevoluteJoint(world, enemyBody.body, wheel.body, Vector2.Zero);
-            axis.CollideConnected = false;
-
-            axis.MotorEnabled = true;
-            axis.MotorSpeed = 0;
-            axis.MotorTorque = 3;
-            axis.MaxMotorTorque = 10;
-
-            numFootContacts = 0;
+            //enemyBody.body.OnCollision += new OnCollisionEventHandler(body_OnCollision);
             
-
-            enemyBody.body.OnCollision += new OnCollisionEventHandler(body_OnCollision);
         }
 
         bool body_OnCollision(Fixture fixtureA, Fixture fixtureB, FarseerPhysics.Dynamics.Contacts.Contact contact)
@@ -123,39 +85,39 @@ namespace Athyl
         {
             if (!hit)
             {
-                if (((int)enemyBody.Position.X - (int)aPlayer.torso.Position.X) < -250 && !seen)
+                if (((int)torso.Position.X - (int)aPlayer.torso.Position.X) < -250 && !seen)
                 {
                     axis.MotorSpeed = 0;
                     UpdateFrame(0.2f);
                 }
 
-                else if (((int)enemyBody.Position.X - (int)aPlayer.torso.Position.X) > 250 && !seen)
+                else if (((int)torso.Position.X - (int)aPlayer.torso.Position.X) > 250 && !seen)
                 {
                     axis.MotorSpeed = 0;
                     UpdateFrame(0.2f);
                 }
 
-                else if (enemyBody.Position.X < aPlayer.torso.Position.X)
+                else if (torso.Position.X < aPlayer.torso.Position.X)
                 {
                     axis.MotorSpeed = MathHelper.TwoPi * speed;
                     UpdateFrame(0.2f);
                     seen = true;
                 }
 
-                else if (enemyBody.Position.X > aPlayer.torso.Position.X)
+                else if (torso.Position.X > aPlayer.torso.Position.X)
                 {
                     axis.MotorSpeed = -MathHelper.TwoPi * speed;
                     UpdateFrame(0.2f);
                     seen = true;
                 }
 
-                if (enemyBody.Position.X > aPlayer.torso.Position.X && enemyBody.Position.Y > aPlayer.torso.Position.Y+10)
+                if (torso.Position.X > aPlayer.torso.Position.X && torso.Position.Y > aPlayer.torso.Position.Y + 10)
                 {
                     
                     if ((DateTime.Now - previousJump).TotalSeconds >= jumpInterval)
                     {
                         axis.MotorSpeed = -MathHelper.TwoPi * speed;
-                        enemyBody.body.ApplyLinearImpulse(jumpForce);
+                        torso.body.ApplyLinearImpulse(jumpForce);
                         previousJump = DateTime.Now;
                         UpdateFrame(0.2f);
                     }
@@ -163,12 +125,12 @@ namespace Athyl
                     
                 }
 
-                else if (enemyBody.Position.X < aPlayer.torso.Position.X && enemyBody.Position.Y > aPlayer.torso.Position.Y+10 && seen)
+                else if (torso.Position.X < aPlayer.torso.Position.X && torso.Position.Y > aPlayer.torso.Position.Y + 10 && seen)
                 {
                     if ((DateTime.Now - previousJump).TotalSeconds >= jumpInterval)
                     {
                          axis.MotorSpeed = MathHelper.TwoPi * speed;
-                         enemyBody.body.ApplyLinearImpulse(jumpForce);
+                         torso.body.ApplyLinearImpulse(jumpForce);
                          previousJump = DateTime.Now;
                          UpdateFrame(0.2f);
                     }
@@ -185,6 +147,7 @@ namespace Athyl
         public void UpdateEnemy(Player aPlayer)
         {
             towardsPlayer(aPlayer);
+            
             //attackPlayer();
         }
         /*
@@ -193,22 +156,69 @@ namespace Athyl
             enemyHP -= damageTaken;
         }
         */
-        public void Draw(SpriteBatch spriteBatch)
+        
+        public override void Draw(SpriteBatch spriteBatch)
         {
-            DrawFrame(spriteBatch, wheel.Position + new Vector2(-55.0f/2, -110f));
+            //DrawFrame(spriteBatch, wheel.Position + new Vector2(-55.0f/2, -110f));
             //enemyBody.Draw(spriteBatch, new Vector2(enemyBody.Size.X, enemyBody.Size.Y + wheel.Size.Y));
             //wheel.Draw(spriteBatch);
-        }
-        public void Load(Texture2D texture, int FrameCount, int FramesPerSec)
-        {
-            framecount = FrameCount;
-            myTexture = texture;
-            TimePerFrame = (float)1 / FramesPerSec;
-            Frame = 11;
-            TotalElapsed = 0;
+
+            base.Draw(spriteBatch);
         }
 
-        public void UpdateFrame(float elapsed)
+        protected override void UpdateFrame(float elapsed)
+        {
+            if (axis.MotorSpeed > 0)
+            {
+                if (ColFrame < 11)
+                    ColFrame = 12;
+
+                TotalElapsed += elapsed;
+                if (TotalElapsed > TimePerFrame)
+                {
+                    ColFrame++;
+                    if (ColFrame == 22)
+                        ColFrame = 12;
+                    TotalElapsed -= TimePerFrame;
+                    runDirection = (int)axis.MotorSpeed;
+                }
+            }
+
+            else if (axis.MotorSpeed < 0)
+            {
+                if (ColFrame > 11)
+                    ColFrame = 9;
+
+                TotalElapsed += elapsed;
+                if (TotalElapsed > TimePerFrame)
+                {
+                    ColFrame--;
+                    if (ColFrame == 0)
+                        ColFrame = 9;
+                    TotalElapsed -= TimePerFrame;
+                    runDirection = (int)axis.MotorSpeed;
+                }
+            }
+
+            else if (axis.MotorSpeed == 0)
+            {
+                if (runDirection > 0)
+                    ColFrame = 12;
+                else if (runDirection < 0)
+                    ColFrame = 10;
+            }
+        }
+
+        //private override Load(Texture2D texture, int FrameCount, int FramesPerSec)
+        //{
+        //    framecount = FrameCount;
+        //    myTexture = texture;
+        //    TimePerFrame = (float)1 / FramesPerSec;
+        //    Frame = 11;
+        //    TotalElapsed = 0;
+        //}
+        
+        /*public void UpdateFrame(float elapsed)
         {
             if (axis.MotorSpeed > 0)
             {
@@ -262,6 +272,6 @@ namespace Athyl
                 FrameWidth, myTexture.Height);
             Batch.Draw(myTexture, screenpos, sourcerect, Color.White,
                 0.0f, new Vector2(0.0f, 0.0f), 1.0f, SpriteEffects.None, 1.0f);
-        }
+        }*/
     }
 }
